b4333ba734c9f1dbfb54b6e48fe6f1ff6fee5124
Ubuntu <ubuntu@ip-172-31-24-199.ec2.internal>
2025-09-12 06:44:42 +0000

    new exp scripts

--- python/exp_1:ours_vs_baselines/run_exp1.sh
+++ python/exp_1:ours_vs_baselines/run_exp1.sh
@@ -1,12 +1,11 @@
 python python/exp_1:ours_vs_baselines/run_payload_trials_parallel.py \
-    --outdir paper_plots/exp1_v4/no_noise_rls_only --ntrials 100 --noise none --save_traj
+    --outdir paper_plots/exp1_v5/no_noise_wind5 --ntrials 100 --noise none --save_traj --window 5
 
 python python/exp_1:ours_vs_baselines/run_payload_trials_parallel.py \
-    --outdir paper_plots/exp1_v4/low_noise_rls_only --ntrials 100 --noise low --save_traj
+    --outdir paper_plots/exp1_v5/low_noise_wind5 --ntrials 100 --noise low --save_traj --window 5
 
 python python/exp_1:ours_vs_baselines/run_payload_trials_parallel.py \
-    --outdir paper_plots/exp1_v4/medium_noise_rls_only --ntrials 100 --noise medium --save_traj
+    --outdir paper_plots/exp1_v5/medium_noise_wind5 --ntrials 100 --noise medium --save_traj --window 5
 
 python python/exp_1:ours_vs_baselines/run_payload_trials_parallel.py \
-    --outdir paper_plots/exp1_v4/high_noise_rls_only --ntrials 100 --noise high --save_traj
-
+    --outdir paper_plots/exp1_v5/high_noise_wind5 --ntrials 100 --noise high --save_traj --window 5
--- python/exp_3:switching_ours_and_baselines/run_exp3.sh
+++ python/exp_3:switching_ours_and_baselines/run_exp3.sh
@@ -1,15 +1,15 @@
 python python/exp_3:switching_ours_and_baselines/run_exp3_parallel.py \
-    --outdir paper_plots/exp3_v0/medium_noise_sub1 --ntrials 100 --noise medium --save_traj \
+    --outdir paper_plots/exp3_v1/medium_noise_sub1_wind5 --ntrials 100 --noise medium --save_traj --window 5 \
     --algos rls_0.99_sub_by_tagrk rls_0.96_sub_by_tagrk kf_high_sub_by_tagrk kf_low_sub_by_tagrk tagrk_sub_by_kf_high tagrk_sub_by_rls_0.96 --sub_k 1
 
 python python/exp_3:switching_ours_and_baselines/run_exp3_parallel.py \
-    --outdir paper_plots/exp3_v0/medium_noise_sub2 --ntrials 100 --noise medium --save_traj \
+    --outdir paper_plots/exp3_v1/medium_noise_sub2_wind5 --ntrials 100 --noise medium --save_traj --window 5 \
     --algos rls_0.99_sub_by_tagrk rls_0.96_sub_by_tagrk kf_high_sub_by_tagrk kf_low_sub_by_tagrk tagrk_sub_by_kf_high tagrk_sub_by_rls_0.96 --sub_k 2
 
-python python/exp_3:switching_ours_and_baselines/run_exp3_parallel.py \
-    --outdir paper_plots/exp3_v0/medium_noise_sub3 --ntrials 100 --noise medium --save_traj \
-    --algos rls_0.99_sub_by_tagrk rls_0.96_sub_by_tagrk kf_high_sub_by_tagrk kf_low_sub_by_tagrk tagrk_sub_by_kf_high tagrk_sub_by_rls_0.96 --sub_k 3
+# python python/exp_3:switching_ours_and_baselines/run_exp3_parallel.py \
+#     --outdir paper_plots/exp3_v0/medium_noise_sub3 --ntrials 100 --noise medium --save_traj \
+#     --algos rls_0.99_sub_by_tagrk rls_0.96_sub_by_tagrk kf_high_sub_by_tagrk kf_low_sub_by_tagrk tagrk_sub_by_kf_high tagrk_sub_by_rls_0.96 --sub_k 3
 
-python python/exp_3:switching_ours_and_baselines/run_exp3_parallel.py \
-    --outdir paper_plots/exp3_v0/medium_noise_sub4 --ntrials 100 --noise medium --save_traj \
-    --algos rls_0.99_sub_by_tagrk rls_0.96_sub_by_tagrk kf_high_sub_by_tagrk kf_low_sub_by_tagrk tagrk_sub_by_kf_high tagrk_sub_by_rls_0.96 --sub_k 4
+# python python/exp_3:switching_ours_and_baselines/run_exp3_parallel.py \
+#     --outdir paper_plots/exp3_v0/medium_noise_sub4 --ntrials 100 --noise medium --save_traj \
+#     --algos rls_0.99_sub_by_tagrk rls_0.96_sub_by_tagrk kf_high_sub_by_tagrk kf_low_sub_by_tagrk tagrk_sub_by_kf_high tagrk_sub_by_rls_0.96 --sub_k 4
